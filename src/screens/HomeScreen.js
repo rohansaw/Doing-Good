b62d5bbb85521d0b8926b11a89f2b0ecfbe64d09
rohansaw <rohan.sawahn@hotmail.de>
2019-06-01 17:42:45 +0200

    homescreen redone

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
-  Image,
   TextInput,
+  Button,
+  FlatList,
   Picker,
-  FlatList
 } from 'react-native';
-
 import firebase from 'firebase';
-import SplashScreen from '../components/SplashScreen';
-import CardStack, { Card } from 'react-native-card-stack-swiper';
-import Colors from '../assets/Colors';
+import { TouchableHighlight, TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 import MatchPreview from '../components/MatchPreview';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { ScrollView } from 'react-native-gesture-handler';
+import SplashScreen from '../components/SplashScreen';
+import Colors from '../assets/Colors';
 
+export default class HomeScreen extends React.Component {
 
-export default class App extends Component {
   constructor(props){
     super(props);
     this.state = ({
@@ -28,234 +24,173 @@ export default class App extends Component {
       loading: true,
       keys: null,
       requests: null,
-      text: '',
-      requests : null,
     });
   }
 
-  componentWillMount(){
-
-    this.focusListener = this.props.navigation.addListener("didFocus", () => { 
-      const {userid} = this.state;
-      firebase.database().ref('users/').child(userid).child('matches').orderByValue().equalTo(1).once('value')
-        .then((snapshot) => {
-          this.setState({
-            keys: snapshot.val(),
-        })
-      });
-      firebase.database().ref('requests').once('value')
+  componentWillMount() {
+   this.focusListener = this.props.navigation.addListener("didFocus", () => { 
+    const {userid} = this.state;
+    firebase.database().ref('users/').child(userid).child('matches').orderByValue().equalTo(1).once('value')
       .then((snapshot) => {
         this.setState({
-          requests: snapshot.val(),
-          loading: false,
-        })
-      });
-     });
-      const {userid} = this.state;
-      firebase.database().ref('users/').child(userid).child('matches').orderByValue().equalTo(1).once('value')
-        .then((snapshot) => {
-          this.setState({
-            keys: snapshot.val(),
-        })
-      });
-      firebase.database().ref('requests').once('value')
+          keys: snapshot.val(),
+      })
+    });
+    firebase.database().ref('requests').once('value')
+    .then((snapshot) => {
+      this.setState({
+        requests: snapshot.val(),
+        loading: false,
+      })
+    });
+   });
+    const {userid} = this.state;
+    firebase.database().ref('users/').child(userid).child('matches').orderByValue().equalTo(1).once('value')
       .then((snapshot) => {
         this.setState({
-          requests: snapshot.val(),
-          loading: false,
-        })
-      });
-      console.log('Sollte vorher');
+          keys: snapshot.val(),
+      })
+    });
+    firebase.database().ref('requests').once('value')
+    .then((snapshot) => {
+      this.setState({
+        requests: snapshot.val(),
+        loading: false,
+      })
+    });
 
+   
   }
+
   componentWillUnmount() {
     this.focusListener.remove();
   }
+    
+    render() {
 
-  match(key){
-    const {userid, users} = this.state;
-    var ref = firebase.database().ref('users').child(userid).child('matches');
-    ref.child(key).set(1);
-    let updates = {};
-    updates['/users/'+userid+'/karmapoints'] = users[userid]['karmapoints']+100;
-    firebase.database().ref().update(updates);
-  }
+      if (this.state.loading){
+        return <SplashScreen/>;
+      }
+      if (this.state.keys == null){
+        return false;
+      }
+      console.log(typeof this.state.keys);
+        return (
+        <View style={styles.container}>
+          <View style={{ marginTop: 20,}}>
+            <View style={styles.searchContainer}>
+              <TextInput
+                placeholder="🔍 Search"
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+                style={styles.search}
+              />
+            </View>              
+          </View>
 
-  decline(key){
-    const {userid} = this.state;
-    var ref = firebase.database().ref('users').child(userid).child('matches');
-    ref.child(key).set(0);
-  }
-
-  render() {
-    const {loading, data, keys, users} = this.state;
-    if(this.state.keys == null ){
-      return false;
-    }
-    //console.log(this.state.keys);
-    //console.log("YYYYYYYYYYYYYYYYYY"+this.state.data)
-    if(loading){
-      return <SplashScreen/>;
-    }
-    return (
-      <View style={{flex:1}}>
-        <View style={styles.searchContainer}>
-          <TextInput
-            placeholder="🔍 Search"
-            onChangeText={(text) => this.setState({text})}
-            value={this.state.text}
-            style={styles.search}
-          />
-          <Picker
-            selectedValue="Category"
-            style={{height: 50, width: 300, fontSize: 11}}>
-            <Picker.Item label="Category" value="Category" />
-            <Picker.Item  label="Haushalt" value="Haushalt" />
-            <Picker.Item label="Kueche" value="Küche" />
-          </Picker>
-        </View>
-
-        <ScrollView>
+          <ScrollView>
             <View style={styles.cardContainer}>
             
-              <FlatList
-                  
-                  data={this.state.keys == null ? [] : Object.keys(this.state.keys).map(key => {
-                    return this.state.request[key];
-                  })}
-                  renderItem={({ item }) => (
-                      
-                      <MatchPreview item={item} />
-                  )}
-                  keyExtractor={(item, index) => index}
-              />
-
+            <FlatList
+                
+                data={this.state.keys == null ? [] : Object.keys(this.state.keys).map(key => {
+                  return this.state.requests[key];
+                })}
+                renderItem={({ item }) => (
+                    <MatchPreview item={item} />
+                )}
+                keyExtractor={(item, index) => index}
+            />
             </View>
+            </ScrollView>
+
+         
             
-        </ScrollView>
+          </View>
         
-      </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#f2f2f2',
-  },
-  content:{
-    flex: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  card:{
-    width: 320,
-    height: 470,
-    backgroundColor: '#FE474C',
-    borderRadius: 5,
-    shadowColor: 'rgba(0,0,0,0.5)',
-    shadowOffset: {
-      width: 0,
-      height: 1
+    container: {
+      flex: 1,
+      width: '100%',
+      backgroundColor: '#fff',
+      height:'100%'
     },
-    shadowOpacity:0.5,
-  },
-  topCard : {
-    width: '100%',
-    borderBottomWidth: 2,
-    marginBottom: 20,
-  },
-  bottomCard : {
-    padding: 10,
-    height:270,
-    width:'100%',
-  },
-  card1: {
-    backgroundColor: '#FE474C',
-  },
-  card2: {
-    backgroundColor: '#FEB12C',
-  },
-  label: {
-    fontSize: 20,
-    fontFamily: 'Roboto',
-    color: '#ffffff',
-    backgroundColor: 'transparent',
-  },
-  footer:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  buttonContainer:{
-    flexDirection:'row',
-    width:'100%',
-    justifyContent: 'center',
-  },
-  button:{
-    shadowColor: 'rgba(0,0,0,0.3)',
-    shadowOffset: {
-      width: 0,
-      height: 1
+    loginContainer : {
+      margin: 30,
     },
-    shadowOpacity:0.5,
-    backgroundColor:'#fff',
-    alignItems:'center',
-    justifyContent:'center',
-    zIndex: 0,
-  },
-  orange:{
-    width:55,
-    height:55,
-    borderWidth:6,
-    borderColor:'rgb(246,190,66)',
-    borderWidth:4,
-    borderRadius:55,
-    marginTop:-15
-  },
-  green:{
-    width:75,
-    height:75,
-    backgroundColor:'#fff',
-    borderRadius:75,
-    borderWidth:3,
-    borderColor:'#01df8a',
-  },
-  red:{
-    width:75,
-    height:75,
-    backgroundColor:'#fff',
-    borderRadius:75,
-    borderWidth:3,
-    borderColor: Colors.sunsetOrange,
-  },
-  bottomItemCard: {
-    position: 'absolute',
-    bottom: 30,
-    paddingLeft: 10,
-  },
-  search : {
-    width: 65 + "%",
-    padding: 10,
-    margin: 20,
-    backgroundColor: '#F5F5F5',
-    color: Colors.grey,
-    borderRadius: 10,
-    fontSize:16
-  },
-  searchIcon : {
-    padding: 10,
-  },
-  searchContainer : {
-    flex: 1,
-    flexDirection: "row",
-    width:"100%",
-    marginLeft: 10,
-    marginRight: 10,
-    alignContent: 'center',
-    textAlign: 'center',
-    alignItems: 'center'
-  }
-});
+    loginItem : {
+      padding: 20,
+      borderRadius: 40,
+      borderWidth: 1,
+      borderColor: '#ddd'
+    },
+    headerContainer : {
+      marginTop: 10,
+      marginBottom: 20,
+    },
+    header : {
+      fontWeight: 'bold',
+      fontSize: 18,
+      textAlign: 'center', 
+      color: Colors.weldonBlue,
+    },
+    input: {
+      backgroundColor: '#fff',
+      borderRadius: 0,
+      borderWidth: 0,
+      borderBottomColor: '#ddd',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginBottom: 20
+    },
+    inputDesc : {
+      backgroundColor: '#fff',
+      borderRadius: 0,
+      borderWidth: 0,
+      borderBottomColor: '#ddd',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginBottom: 20,
+      height: 70,
+    },
+    bottomContainer : {
+      width: '100%',
+      position: 'absolute',
+      bottom:0,
+      
+    },
+    bottomItem : {
+      width: '100%',
+      backgroundColor : 'blue',
+      height:50,
+      justifyContent: 'center'
+    },
+    signUpItem : {
+      textAlign:'center',
+      justifyContent:'center',
+      
+    },
+    cardContainer : {
+      margin: 20,
+      marginBottom:50,
+    },
+    search : {
+      width: 100 + "%",
+      padding: 10,
+      marginBottom:0,
+      backgroundColor: '#F5F5F5',
+      color: Colors.grey,
+      borderRadius: 10,
+      fontSize:16
+    },
+    searchIcon : {
+      padding: 10,
+    },
+    searchContainer: {
+      padding: 20,
+    }
+
+  });
+  
